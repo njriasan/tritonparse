e15f5eb9f88aad781ffb32c1fedde05a64065e82
FindHao <findhao@outlook.com>
2025-10-13 12:45:47 -0400

    Refactor KernelImportMode Enum Documentation

--- tritonparse/reproducer/types.py
+++ tritonparse/reproducer/types.py
@@ -8,12 +8,8 @@ class KernelImportMode(str, Enum):
     Inherits from str to allow direct string comparison and use in argparse.
     """
 
-    DEFAULT = "default"
-    """Import kernel from original file (current behavior)."""
-
-    COPY = "copy"
-    """Embed kernel source code directly in reproducer."""
+    DEFAULT = "default"  # Import kernel from original file (current behavior)
+    COPY = "copy"  # Embed kernel source code directly in reproducer
 
     # Future modes can be added here:
-    # OVERRIDE_TTIR = "override-ttir"
-    # """Use TTIR from compilation event with monkeypatch."""
+    # OVERRIDE_TTIR = "override-ttir"  # Use TTIR from compilation event with monkeypatch
